653a74efa618bf072c7bc881893cad8453eb8902
kingkeya4-pixel <kingkeya4@gmail.com>
2026-08-20 13:33:12 +0530

    Largest of 2 Numbers

---
+++ PrimeNumber.java
@@ -0,0 +1,21 @@
+public class PrimeNumber {
+    public static void main(String[] args) {
+        int n = 17;
+        boolean prime = true;
+
+        if (n < 2)
+            prime = false;
+
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                prime = false;
+                break;
+            }
+        }
+
+        if (prime)
+            System.out.println("Prime Number");
+        else
+            System.out.println("Not a Prime Number");
+    }
+}
