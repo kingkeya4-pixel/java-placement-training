df6e680f6622994eee4553a1642261ee76ad5abb
kingkeya4-pixel <kingkeya4@gmail.com>
2026-08-20 13:25:39 +0530

    Largest of 2 Numbers

---
+++ LargestOfThree .java
@@ -0,0 +1,14 @@
+public class LargestOfThree {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 30;
+        int c = 20;
+
+        if (a >= b && a >= c)
+            System.out.println(a);
+        else if (b >= a && b >= c)
+            System.out.println(b);
+        else
+            System.out.println(c);
+    }
+}
